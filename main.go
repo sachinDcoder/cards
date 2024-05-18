e6c1b6c5b3ea84cc15c26013073ee39e84667612
Raja Sachin <sachinra@vmware.com>
2024-05-18 23:59:54 +0530

    Added receiver

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Six of Diamonds", newCards()}
 	cards = append(cards, "Five of Ace")
-	//fmt.Println(cards)
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCards() string {
