d684c8a8f5c86304ac86835b18fbe30d8467c108
Raja Sachin <sachinra@vmware.com>
2024-05-19 21:32:23 +0530

    Added more tests

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected length of deck is 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of \"%s\", but got %s", "Ace of Spades", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of \"%s\", but got %s", "Four of Clubs", d[len(d)-1])
 	}
 }
