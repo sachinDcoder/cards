7481039ed584dc4dd2f33a5be86d1cd78ee9533c
Raja Sachin <sachinra@vmware.com>
2024-05-19 21:16:59 +0530

    Added first test func

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	// err := cards.writeToFile("my-cards.txt")
-	// if err != nil {
-	// 	fmt.Println("Error: ", err)
-	// }
-
-	cards := newDeckFromFile("my-cards.txt")
-	//cards.print()
-
+	cards := newDeck()
 	cards.shuffle()
 	cards.print()
 }
