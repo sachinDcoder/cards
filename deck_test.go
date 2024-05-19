82e0f3d0568f4befdf5cbe388771800cc2c689a4
Raja Sachin <sachinra@vmware.com>
2024-05-19 21:57:44 +0530

    Added test for deal

--- deck_test.go
+++ deck_test.go
@@ -35,3 +35,17 @@ func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
 
 	os.Remove("_decktesting")
 }
+
+func TestDeal(t *testing.T) {
+	d := newDeck()
+
+	d1, d2 := deal(d, 5)
+
+	if len(d1) != 5 || len(d2) != 11 {
+		t.Errorf("Expected length of 5 in d1, but got %v", len(d1))
+	}
+
+	if len(d2) != 11 {
+		t.Errorf("Expected length of 11 in d1, but got %v", len(d2))
+	}
+}
